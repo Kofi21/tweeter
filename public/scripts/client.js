d72fc48ae71582b19794afbdf662f7fd76c583cf
Evander Owusu <owu.evander@gmail.com>
2021-12-16 01:28:52 +0000

    create function for createTweetElement and renderTweets so tweets are rendered dynamically

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,62 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function () {
+  const renderTweets = function (tweets) {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $("#tweets-container").append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+    }
+  };
+
+  const createTweetElement = function (tweet) {
+    const date = timeago.format(tweet.created_at);
+    return ` 
+            <article class="tweet">
+          <header class="tweet-header">
+          <img src=${tweet.user.avatars} />
+            <div class="username">${tweet.user.name}</div>
+            <div class="handle">${tweet.user.handle}</div>
+          </header>
+          <p>${tweet.content.text}</p>
+          <footer class="tweet-footer">
+            <div>${date}</div>
+            <div class="icons">
+              <i class="fa-solid fa-flag"></i>
+              <i class="fa-solid fa-retweet"></i>
+              <i class="fa-solid fa-heart"></i>
+            </div>
+          </footer>
+        </article>
+    `;
+  };
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
+      },
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
+      },
+      created_at: 1461116232227,
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
+      },
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1461113959088,
+    },
+  ];
+
+  // const $tweet = createTweetElement(tweetData);
+
+  renderTweets(data);
+});
