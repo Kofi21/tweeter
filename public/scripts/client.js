2423bf2a9dd0934979a83cfd30c5b75e31cbefc4
Evander Owusu <owu.evander@gmail.com>
2021-12-16 23:27:00 +0000

    deleted helpers.js and simplified code to make it readable

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,51 +1,73 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
 $(document).ready(function () {
-  loadTweets();
+  const renderTweets = function (tweets) {
+    $("#tweets-container").empty();
+    for (const tweet of tweets) {
+      createTweetElement(tweet);
+    }
+  };
+
+  const createTweetElement = function (tweet) {
+    const date = timeago.format(tweet.created_at);
+
+    let $tweet = $(` 
+            <article class="tweet">
+          <header class="tweet-header">
+          <img src=${tweet.user.avatars} />
+            <div class="username">${tweet.user.name}</div>
+            <div class="handle">${tweet.user.handle}</div>
+          </header>
+          <p>${tweet.content.text}</p>
+          <footer class="tweet-footer">
+            <div>${date}</div>
+            <div class="icons">
+              <i class="fa-solid fa-flag"></i>
+              <i class="fa-solid fa-retweet"></i>
+              <i class="fa-solid fa-heart"></i>
+            </div>
+          </footer>
+        </article>
+    `);
+    $("#tweets-container").prepend($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  };
+
+  const reset = function () {
+    const counter = $(".counter");
+    const tweetTextBox = $("form").find("#tweet-text");
+    counter.html("140");
+    tweetTextBox.val("");
+  };
+
   $("form").submit(function (event) {
     event.preventDefault();
-    const tweetTextValue = $(this).find("tweet-text").val().trim();
-    if (errorCheck(tweetTextValue)) {
+    const tweetTextValue = $(this).find("#tweet-text").val().trim();
+
+    if (tweetTextValue === "" || null) {
+      alert("Tweet something");
       return;
     }
-    $.ajax("/tweets", {
+    if (tweetTextValue.length > 140) {
+      alert("Tweets are limited to 140 characters!");
+      return;
+    }
+
+    $.ajax({
+      url: "/tweets",
       method: "POST",
       data: $(this).serialize(),
+    }).then(function () {
+      reset();
+      loadTweets();
     });
-    console.log(event);
   });
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  // const data = [
-  //   {
-  //     user: {
-  //       name: "Newton",
-  //       avatars: "https://i.imgur.com/73hZDYK.png",
-  //       handle: "@SirIsaac",
-  //     },
-  //     content: {
-  //       text: "If I have seen further it is by standing on the shoulders of giants",
-  //     },
-  //     created_at: 1461116232227,
-  //   },
-  //   {
-  //     user: {
-  //       name: "Descartes",
-  //       avatars: "https://i.imgur.com/nlhLi3I.png",
-  //       handle: "@rd",
-  //     },
-  //     content: {
-  //       text: "Je pense , donc je suis",
-  //     },
-  //     created_at: 1461113959088,
-  //   },
-  // ];
+  const loadTweets = function () {
+    $.ajax("/tweets", {
+      method: "GET",
+      data: "JSON",
+    }).then(function (tweet) {
+      renderTweets(tweet);
+    });
+  };
 
-  // const $tweet = createTweetElement(tweetData);
-
-  // renderTweets(data);
+  loadTweets();
 });
